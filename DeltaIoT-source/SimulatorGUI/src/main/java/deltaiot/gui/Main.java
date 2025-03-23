a2cd262c97d3cf139fd5c800fa35fe42dd0f9eae
Ralf Sieger <sieger@fzi.de>
2025-03-23 06:39:49 +0100

    simulator GUI running

---
+++ DeltaIoT-source/SimulatorGUI/src/main/java/deltaiot/gui/Main.java
@@ -0,0 +1,10 @@
+package deltaiot.gui;
+
+import javafx.application.Application;
+
+public class Main {
+	public static void main(String[] args) {
+		Application.launch(DeltaIoTEmulatorMain.class, args);
+	}
+
+}
