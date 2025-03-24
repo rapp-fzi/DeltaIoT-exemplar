aaee4c4edde5ad8e88ec1b1950875b7c99615e19
Martina Rapp <rapp@fzi.de>
2025-03-24 16:45:36 +0100

    Add csv file writer to save simulation results and configurations.

--- DeltaIoT-source/SimpleAdaptationWithSimulation/src/main/java/main/SimpleAdaptation.java
+++ DeltaIoT-source/SimpleAdaptationWithSimulation/src/main/java/main/SimpleAdaptation.java
@@ -8,42 +8,46 @@ import deltaiot.client.SimulationClient;
 import mapek.FeedbackLoop;
 import simulator.QoS;
 import simulator.Simulator;
+import util.CsvFileWriter;
 
 public class SimpleAdaptation {
-	
-	SimulationClient networkMgmt;
-	public void start(){
-		
-		// Create a simulation client object		
-		networkMgmt = new SimulationClient();
-		
-		// Create Feedback loop
-		FeedbackLoop feedbackLoop = new FeedbackLoop();
 
-		// get probe and effectors
-		Probe probe = networkMgmt.getProbe();
-		Effector effector = networkMgmt.getEffector();
+    SimulationClient networkMgmt;
 
-		// Connect probe and effectors with feedback loop
-		feedbackLoop.setProbe(probe);
-		feedbackLoop.setEffector(effector);
+    public void start() {
 
-		// StartFeedback loop
-		feedbackLoop.start();
-		
-		ArrayList<QoS> result = networkMgmt.getNetworkQoS(96);
-		
-		System.out.println("Run, PacketLoss, EnergyConsumption");
-		result.forEach(qos -> System.out.println(qos));
+        // Create a simulation client object
+        networkMgmt = new SimulationClient();
 
-	}
-	
-	public static void main(String[] args) {
-		SimpleAdaptation client = new SimpleAdaptation();
-		client.start();
- 	}
+        // Create Feedback loop
+        FeedbackLoop feedbackLoop = new FeedbackLoop();
 
-	public Simulator getSimulator() {
-		return networkMgmt.getSimulator();
-	}
+        // get probe and effectors
+        Probe probe = networkMgmt.getProbe();
+        Effector effector = networkMgmt.getEffector();
+
+        // Connect probe and effectors with feedback loop
+        feedbackLoop.setProbe(probe);
+        feedbackLoop.setEffector(effector);
+
+        // StartFeedback loop
+        feedbackLoop.start();
+
+        ArrayList<QoS> result = networkMgmt.getNetworkQoS(96);
+
+        System.out.println("Run, PacketLoss, EnergyConsumption");
+        result.forEach(qos -> System.out.println(qos));
+
+        CsvFileWriter.saveQoS(result, feedbackLoop.getId());
+
+    }
+
+    public static void main(String[] args) {
+        SimpleAdaptation client = new SimpleAdaptation();
+        client.start();
+    }
+
+    public Simulator getSimulator() {
+        return networkMgmt.getSimulator();
+    }
 }
