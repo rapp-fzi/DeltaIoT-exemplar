9c987eb934d9b3a5c7a22d308ee78e2ba582f57b
Martina Rapp <rapp@fzi.de>
2025-03-24 13:47:57 +0100

    fixed path issue on windows

--- DeltaIoT-source/SimulatorGUI/src/main/java/deltaiot/gui/DeltaIoTEmulatorMain.java
+++ DeltaIoT-source/SimulatorGUI/src/main/java/deltaiot/gui/DeltaIoTEmulatorMain.java
@@ -1,5 +1,14 @@
 package deltaiot.gui;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.LinkedList;
+import java.util.List;
+
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.concurrent.Service;
@@ -11,6 +20,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -20,235 +30,235 @@ import javafx.stage.Stage;
 import main.SimpleAdaptation;
 import simulator.QoS;
 import simulator.Simulator;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.net.URL;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.LinkedList;
-import java.util.List;
-import javafx.scene.chart.LineChart;
 
 public class DeltaIoTEmulatorMain extends Application {
-	@FXML
-	private Button runEmulator, btnSaveResults, btnAdaptationLogic, btnClearResults, btnDisplay;
+    @FXML
+    private Button runEmulator, btnSaveResults, btnAdaptationLogic, btnClearResults, btnDisplay;
 
-	@FXML
-	private LineChart<?, ?> chartPacketLoss, chartEnergyConsumption;
+    @FXML
+    private LineChart<?, ?> chartPacketLoss, chartEnergyConsumption;
 
-	@FXML
-	private ProgressBar progressBar;
+    @FXML
+    private ProgressBar progressBar;
 
-	@FXML
-	private Label lblProgress;
+    @FXML
+    private Label lblProgress;
 
-	public static final Path filePath = Paths.get(System.getProperty("user.dir") + "/data/data.csv");
-	List<String> data = new LinkedList<String>();
-	Stage primaryStage;
-	Simulator simul;
-	Service serviceEmulation = new Service() {
+    public static final Path filePath = Paths.get(System.getProperty("user.dir") + "/data/data.csv");
+    List<String> data = new LinkedList<>();
+    Stage primaryStage;
+    Simulator simul;
+    Service serviceEmulation = new Service() {
 
-		@Override
-		protected void succeeded() {
-			displayData("Without Adaptation", 0);
-		}
+        @Override
+        protected void succeeded() {
+            displayData("Without Adaptation", 0);
+        }
 
-		@Override
-		protected Task createTask() {
-			return new Task() {
-				@Override
-				protected Object call() throws Exception {
-					btnDisplay.setDisable(true);
-					simul = deltaiot.DeltaIoTSimulator.createSimulatorForDeltaIoT();
-					for (int i = 0; i < 96; i++)
-						simul.doSingleRun();
-					btnDisplay.setDisable(false);
-					return null;
-				}
-			};
-		}
-	};
+        @Override
+        protected Task createTask() {
+            return new Task() {
+                @Override
+                protected Object call() throws Exception {
+                    btnDisplay.setDisable(true);
+                    simul = deltaiot.DeltaIoTSimulator.createSimulatorForDeltaIoT();
+                    for (int i = 0; i < 96; i++)
+                        simul.doSingleRun();
+                    btnDisplay.setDisable(false);
+                    return null;
+                }
+            };
+        }
+    };
 
-	@FXML
-	void runEmulatorClicked(ActionEvent event) {
-		if (!serviceEmulation.isRunning()) {
-			serviceEmulation.restart();
-			serviceProgress.restart();
-		}
-	}
+    @FXML
+    void runEmulatorClicked(ActionEvent event) {
+        if (!serviceEmulation.isRunning()) {
+            serviceEmulation.restart();
+            serviceProgress.restart();
+        }
+    }
 
-	Service serviceProgress = new Service() {
-		@Override
-		protected void succeeded() {
+    Service serviceProgress = new Service() {
+        @Override
+        protected void succeeded() {
 
-		}
+        }
 
-		@Override
-		protected Task createTask() {
-			return new Task() {
-				@Override
-				protected Object call() throws Exception {
-					int run;
-					do {
-						run = simul.getRunInfo().getRunNumber();
+        @Override
+        protected Task createTask() {
+            return new Task() {
+                @Override
+                protected Object call() throws Exception {
+                    int run;
+                    do {
+                        run = simul.getRunInfo()
+                            .getRunNumber();
 
-						updateProgress(run, 96);
-						updateMessage("(" + run + "/96" + ")");
+                        updateProgress(run, 96);
+                        updateMessage("(" + run + "/96" + ")");
 
-						try {
-							Thread.sleep(1000);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
 
-					} while (run < 96);
-					
-					return null;
-				}
-			};
-		}
-	};
+                    } while (run < 96);
 
-	Service serviceAdaptation = new Service() {
+                    return null;
+                }
+            };
+        }
+    };
 
-		@Override
-		protected void succeeded() {
-			displayData("With Adaptation", 1);
-		}
+    Service serviceAdaptation = new Service() {
 
-		@Override
-		protected Task createTask() {
-			return new Task() {
-				@Override
-				protected Object call() throws Exception {
-					btnDisplay.setDisable(true);
-					SimpleAdaptation client = new SimpleAdaptation();
-					client.start();
-					simul = client.getSimulator();
-					btnDisplay.setDisable(false);
-					return null;
-				}
-			};
-		}
-	};
+        @Override
+        protected void succeeded() {
+            displayData("With Adaptation", 1);
+        }
 
-	// ActivFORMSDeploy client;
+        @Override
+        protected Task createTask() {
+            return new Task() {
+                @Override
+                protected Object call() throws Exception {
+                    btnDisplay.setDisable(true);
+                    SimpleAdaptation client = new SimpleAdaptation();
+                    client.start();
+                    simul = client.getSimulator();
+                    btnDisplay.setDisable(false);
+                    return null;
+                }
+            };
+        }
+    };
 
-	@FXML
-	void btnAdaptationLogic(ActionEvent event) {
-		if (!serviceAdaptation.isRunning()) {
-			serviceAdaptation.restart();
-			serviceProgress.restart();
-		}
-	}
+    // ActivFORMSDeploy client;
 
-	@FXML
-	void btnDisplay(ActionEvent event) {
-		try {
-			URL url = new URL("file://" + System.getProperty("user.dir") + "/resources/DeltaIoTModel.fxml");
-			FXMLLoader fxmlLoader = new FXMLLoader(url);
-			Parent root1 = (Parent) fxmlLoader.load();
-			DeltaIoTClientMain main = fxmlLoader.getController();
-			main.setSimulationClient(simul);
-			Stage stage = new Stage();
-			stage.setScene(new Scene(root1));
-			stage.setResizable(false);
-			stage.setTitle("DeltaIoT Topology");
-			stage.setAlwaysOnTop(true);
-			stage.showAndWait();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    @FXML
+    void btnAdaptationLogic(ActionEvent event) {
+        if (!serviceAdaptation.isRunning()) {
+            serviceAdaptation.restart();
+            serviceProgress.restart();
+        }
+    }
 
-	@FXML
-	void initialize() {
-		assert progressBar != null : "fx:id=\"progressBar\" was not injected: check your FXML file 'Progress.fxml'.";
-		lblProgress.textProperty().bind(serviceProgress.messageProperty());
-		progressBar.progressProperty().bind(serviceProgress.progressProperty());
-	}
+    @FXML
+    void btnDisplay(ActionEvent event) {
+        try {
+            URL url = new URL("file://" + System.getProperty("user.dir") + "/resources/DeltaIoTModel.fxml");
+            FXMLLoader fxmlLoader = new FXMLLoader(url);
+            Parent root1 = (Parent) fxmlLoader.load();
+            DeltaIoTClientMain main = fxmlLoader.getController();
+            main.setSimulationClient(simul);
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root1));
+            stage.setResizable(false);
+            stage.setTitle("DeltaIoT Topology");
+            stage.setAlwaysOnTop(true);
+            stage.showAndWait();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	@FXML
-	void btnSaveResults(ActionEvent event) {
-		FileChooser fileChooser = new FileChooser();
+    @FXML
+    void initialize() {
+        assert progressBar != null : "fx:id=\"progressBar\" was not injected: check your FXML file 'Progress.fxml'.";
+        lblProgress.textProperty()
+            .bind(serviceProgress.messageProperty());
+        progressBar.progressProperty()
+            .bind(serviceProgress.progressProperty());
+    }
 
-		// Set extension filter
-		FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
-		fileChooser.getExtensionFilters().add(extFilter);
+    @FXML
+    void btnSaveResults(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
 
-		// Show save file dialog
-		File file = fileChooser.showSaveDialog(primaryStage);
+        // Set extension filter
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("CSV files (*.csv)", "*.csv");
+        fileChooser.getExtensionFilters()
+            .add(extFilter);
 
-		if (file != null) {
-			saveFile(file);
-		}
-	}
+        // Show save file dialog
+        File file = fileChooser.showSaveDialog(primaryStage);
 
-	private void saveFile(File file) {
-		try {
-			FileWriter fileWriter = null;
+        if (file != null) {
+            saveFile(file);
+        }
+    }
 
-			fileWriter = new FileWriter(file, true);
+    private void saveFile(File file) {
+        try {
+            FileWriter fileWriter = null;
 
-			for (String line : data)
-				fileWriter.write(line + "\n");
-			fileWriter.close();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		}
+            fileWriter = new FileWriter(file, true);
 
-	}
+            for (String line : data)
+                fileWriter.write(line + "\n");
+            fileWriter.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
 
-	@FXML
-	void btnClearResults(ActionEvent event) {
-		chartEnergyConsumption.getData().clear();
-		chartPacketLoss.getData().clear();
-		data.clear();
-	}
+    }
 
-	void displayData(String setName, int index) {
-		XYChart.Series energyConsumptionSeries = new XYChart.Series();
-		XYChart.Series packetLossSeries = new XYChart.Series();
-		energyConsumptionSeries.setName(setName);
-		packetLossSeries.setName(setName);
-		List<QoS> qosList = simul.getQosValues();
+    @FXML
+    void btnClearResults(ActionEvent event) {
+        chartEnergyConsumption.getData()
+            .clear();
+        chartPacketLoss.getData()
+            .clear();
+        data.clear();
+    }
 
-		for (QoS qos : qosList) {
-			data.add(qos + ", " + setName);
-			energyConsumptionSeries.getData()
-					.add(new XYChart.Data(Integer.parseInt(qos.getPeriod()), qos.getEnergyConsumption()));
-			packetLossSeries.getData().add(new XYChart.Data(Integer.parseInt(qos.getPeriod()), qos.getPacketLoss()));
-		}
-		chartEnergyConsumption.getData().add(energyConsumptionSeries);
-		chartPacketLoss.getData().add(packetLossSeries);
-	}
+    void displayData(String setName, int index) {
+        XYChart.Series energyConsumptionSeries = new XYChart.Series();
+        XYChart.Series packetLossSeries = new XYChart.Series();
+        energyConsumptionSeries.setName(setName);
+        packetLossSeries.setName(setName);
+        List<QoS> qosList = simul.getQosValues();
 
-	@Override
-	public void start(Stage stage) throws Exception {
-		URL url = new URL("file://" + System.getProperty("user.dir") + "/resources/EmulatorGUI.fxml");
-		Parent root = FXMLLoader.load(url);// getClass().getResource(System.getProperty("user.dir")
-											// +
-											// "/resources/EmulatorGUI.fxml"));
-		primaryStage = stage;
-		Scene scene = new Scene(root, 900, 600);
+        for (QoS qos : qosList) {
+            data.add(qos + ", " + setName);
+            energyConsumptionSeries.getData()
+                .add(new XYChart.Data(Integer.parseInt(qos.getPeriod()), qos.getEnergyConsumption()));
+            packetLossSeries.getData()
+                .add(new XYChart.Data(Integer.parseInt(qos.getPeriod()), qos.getPacketLoss()));
+        }
+        chartEnergyConsumption.getData()
+            .add(energyConsumptionSeries);
+        chartPacketLoss.getData()
+            .add(packetLossSeries);
+    }
 
-		stage.setTitle("DeltaIoT");
-		stage.setOnCloseRequest(new EventHandler() {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Path userPath = Paths.get(System.getProperty("user.dir"));
+        Path guifxml = userPath.resolve("resources")
+            .resolve("EmulatorGUI.fxml");
+        URL url = guifxml.toUri()
+            .toURL();
+        Parent root = FXMLLoader.load(url);
+        primaryStage = stage;
+        Scene scene = new Scene(root, 900, 600);
 
-			@Override
-			public void handle(Event arg0) {
-				Platform.exit();
-			}
+        stage.setTitle("DeltaIoT");
+        stage.setOnCloseRequest(new EventHandler() {
 
-		});
-		stage.setScene(scene);
-		stage.show();
-	}
+            @Override
+            public void handle(Event arg0) {
+                Platform.exit();
+            }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+        });
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
