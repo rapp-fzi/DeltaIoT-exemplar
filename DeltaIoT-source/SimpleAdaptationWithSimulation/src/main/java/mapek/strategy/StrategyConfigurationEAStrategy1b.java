c7eb9a4b726aec6743df0e2da7623560b89993d9
Ralf Sieger <sieger@fzi.de>
2025-09-22 13:39:55 +0200

    added range comments

--- DeltaIoT-source/SimpleAdaptationWithSimulation/src/main/java/mapek/strategy/StrategyConfigurationEAStrategy1b.java
+++ DeltaIoT-source/SimpleAdaptationWithSimulation/src/main/java/mapek/strategy/StrategyConfigurationEAStrategy1b.java
@@ -1,5 +1,5 @@
 package mapek.strategy;
 
 public class StrategyConfigurationEAStrategy1b extends StrategyConfigurationEAStrategy1a {
-    int CHANGE_DIST_VALUE;
+    int CHANGE_DIST_VALUE; // optimizable int[1,5,1] closed (stop included)
 }
