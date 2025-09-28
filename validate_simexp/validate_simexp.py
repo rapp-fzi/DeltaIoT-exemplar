043adb733d731eccd6d6f6db1121dddc947db87f
Ralf Sieger <sieger@fzi.de>
2025-09-28 10:01:35 +0200

    added CSV parsing

--- validate_simexp/validate_simexp.py
+++ validate_simexp/validate_simexp.py
@@ -1,10 +1,28 @@
 import argparse
+import csv
 
 class ValidateSimexp:
+
+    def _read_csv_file(self, csv_file):
+        fieldnames = ["Generation","Reward","Values"]
+        reader = csv.DictReader(csv_file, fieldnames=fieldnames, delimiter=";")
+        next(reader) # skip header
+        result = []
+        for row in reader:
+            generation = int(row['Generation'])
+            reward = row['Reward']
+            values = row['Values']
+            result.append({"Generation": generation, "Reward": reward, "Values": values})
+        return result
+
     def main(self):
         parser = argparse.ArgumentParser(prog="validate_simexp", description="Validates SimExp results")
-        #parser.add_argument('infile', type=argparse.FileType('r'))
+        parser.add_argument('infile', type=argparse.FileType('r'))
         args = parser.parse_args()
+
+        entries = self._read_csv_file(args.infile)
+        for entry in entries:
+            print("generation: %d -> %s" % (entry["Generation"], entry["Reward"]))
 
 
 if __name__ == '__main__':
