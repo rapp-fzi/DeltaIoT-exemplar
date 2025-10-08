52ea04b4f4b3130a2d2015fe02fe01e15fdd3501
Ralf Sieger <sieger@fzi.de>
2025-10-08 13:32:49 +0200

    switched to execute pytest

--- DeltaIoT-source/border_test/border_test.cmd
+++ DeltaIoT-source/border_test/border_test.cmd
@@ -6,5 +6,6 @@ set SCRIPT_DIR=%SCRIPT_PATH:~0,-1%
 
 set PYTHON=%SCRIPT_DIR%\venv\Scripts\python
 
-set PY=%SCRIPT_DIR%\border_test.py
-"%PYTHON%" -O "%PY%" %*
+rem set PY=%SCRIPT_DIR%\border_test.py
+rem "%PYTHON%" -O "%PY%" %*
+pytest %*
--- DeltaIoT-source/border_test/border_test.sh
+++ DeltaIoT-source/border_test/border_test.sh
@@ -3,5 +3,6 @@ script_dir=`dirname $(readlink -f "$0")`
 #echo script dir: $script_dir
 activate=$script_dir/venv/bin/activate
 . $activate
-py=$script_dir/border_test.py
-python -O $py "$@"
+#py=$script_dir/border_test.py
+#python -O $py "$@"
+pytest "$@"
