2767e765bf8365717f740e93d4af9f085a771a91
Ralf Sieger <sieger@fzi.de>
2025-10-29 09:14:18 +0100

    fixed CSV reading

--- validate_simexp/input_type.py
+++ validate_simexp/input_type.py
@@ -14,19 +14,19 @@ class Input:
 def _read_csv_file(csv_file):
     fieldnames = ["Generation", "Reward", "Values"]
     with csv_file.open("r", encoding="utf-8") as f:
-        reader = csv.DictReader(csv_file, fieldnames=fieldnames, delimiter=";")
-    next(reader)  # skip header
-    result = []
-    for row in reader:
-        generation = int(row['Generation'])
-        reward = row['Reward']
-        raw_values = row['Values'].split(",")
-        values = {}
-        for raw_value in raw_values:
-            name, val = raw_value.split(":")
-            values[name] = val.strip()
-        result.append({"Generation": generation, "Reward": reward, "Values": values})
-    return result
+        reader = csv.DictReader(f, fieldnames=fieldnames, delimiter=";")
+        next(reader)  # skip header
+        result = []
+        for row in reader:
+            generation = int(row['Generation'])
+            reward = row['Reward']
+            raw_values = row['Values'].split(",")
+            values = {}
+            for raw_value in raw_values:
+                name, val = raw_value.split(":")
+                values[name] = val.strip()
+            result.append({"Generation": generation, "Reward": reward, "Values": values})
+        return result
 
 
 def _read_json_file(json_file):
