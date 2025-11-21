35a1929775590a8da9e5003176223c050c5797dd
Ralf Sieger <sieger@fzi.de>
2025-11-21 13:21:45 +0100

    added tests for EAStrategy2b

--- DeltaIoT-source/border_test/border_test.py
+++ DeltaIoT-source/border_test/border_test.py
@@ -156,3 +156,58 @@ def test_strategy2a(jar_file, cli_path, tmp_path,
             "POWER_MIN_MAX_DELTA": POWER_MIN_MAX_DELTA,
     }
     _execute_simulator(jar_file, cli_path, strategy, config, tmp_path)
+
+@pytest.mark.parametrize('CHANGE_POWER_VALUE1', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE2', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE3', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE4', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE5', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE6', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE7', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE8', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE9', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE10', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE11', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE12', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE13', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE14', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE15', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE16', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE17', [1,  4])
+@pytest.mark.parametrize('POWER_MIN', [0, 5])
+@pytest.mark.parametrize('POWER_MIN_MAX_DELTA', [4, 10])
+@pytest.mark.parametrize('CHANGE_DIST_VALUE', [1, 5])
+def test_strategy2b(jar_file, cli_path, tmp_path,
+                    CHANGE_POWER_VALUE1, CHANGE_POWER_VALUE2, CHANGE_POWER_VALUE3, CHANGE_POWER_VALUE4,
+                    CHANGE_POWER_VALUE5, CHANGE_POWER_VALUE6, CHANGE_POWER_VALUE7, CHANGE_POWER_VALUE8,
+                    CHANGE_POWER_VALUE9, CHANGE_POWER_VALUE10, CHANGE_POWER_VALUE11, CHANGE_POWER_VALUE12,
+                    CHANGE_POWER_VALUE13, CHANGE_POWER_VALUE14, CHANGE_POWER_VALUE15, CHANGE_POWER_VALUE16,
+                    CHANGE_POWER_VALUE17,
+                    POWER_MIN,
+                    POWER_MIN_MAX_DELTA,
+                    CHANGE_DIST_VALUE
+                    ):
+    strategy = "EAStrategy2b"
+    config = {
+            "CHANGE_POWER_VALUE1": CHANGE_POWER_VALUE1,
+            "CHANGE_POWER_VALUE2": CHANGE_POWER_VALUE2,
+            "CHANGE_POWER_VALUE3": CHANGE_POWER_VALUE3,
+            "CHANGE_POWER_VALUE4": CHANGE_POWER_VALUE4,
+            "CHANGE_POWER_VALUE5": CHANGE_POWER_VALUE5,
+            "CHANGE_POWER_VALUE6": CHANGE_POWER_VALUE6,
+            "CHANGE_POWER_VALUE7": CHANGE_POWER_VALUE7,
+            "CHANGE_POWER_VALUE8": CHANGE_POWER_VALUE8,
+            "CHANGE_POWER_VALUE9": CHANGE_POWER_VALUE9,
+            "CHANGE_POWER_VALUE10": CHANGE_POWER_VALUE10,
+            "CHANGE_POWER_VALUE11": CHANGE_POWER_VALUE11,
+            "CHANGE_POWER_VALUE12": CHANGE_POWER_VALUE12,
+            "CHANGE_POWER_VALUE13": CHANGE_POWER_VALUE13,
+            "CHANGE_POWER_VALUE14": CHANGE_POWER_VALUE14,
+            "CHANGE_POWER_VALUE15": CHANGE_POWER_VALUE15,
+            "CHANGE_POWER_VALUE16": CHANGE_POWER_VALUE16,
+            "CHANGE_POWER_VALUE17": CHANGE_POWER_VALUE17,
+            "POWER_MIN": POWER_MIN,
+            "POWER_MIN_MAX_DELTA": POWER_MIN_MAX_DELTA,
+            "CHANGE_DIST_VALUE": CHANGE_DIST_VALUE,
+    }
+    _execute_simulator(jar_file, cli_path, strategy, config, tmp_path)
