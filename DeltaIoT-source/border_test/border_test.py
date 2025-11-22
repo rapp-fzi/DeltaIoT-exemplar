a43a1ac8e77005c6cd0d2df0227bea1df8b4438d
Ralf Sieger <sieger@fzi.de>
2025-11-22 15:34:15 +0100

    added tests for EAStrategy3b and EAStrategy3c

--- DeltaIoT-source/border_test/border_test.py
+++ DeltaIoT-source/border_test/border_test.py
@@ -299,3 +299,187 @@ def test_strategy3a(jar_file, cli_path, tmp_path,
             "POWER_MIN_MAX_DELTA": POWER_MIN_MAX_DELTA,
     }
     _execute_simulator(jar_file, cli_path, strategy, config, tmp_path)
+
+
+@pytest.mark.parametrize('CHANGE_POWER_VALUE1', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE2', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE3', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE4', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE5', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE6', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE7', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE8', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE9', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE10', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE11', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE12', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE13', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE14', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE15', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE16', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE17', [1,  4])
+@pytest.mark.parametrize('POWER_MIN1', [0, 5])
+@pytest.mark.parametrize('POWER_MIN2', [0, 5])
+@pytest.mark.parametrize('POWER_MIN3', [0, 5])
+@pytest.mark.parametrize('POWER_MIN4', [0, 5])
+@pytest.mark.parametrize('POWER_MIN5', [0, 5])
+@pytest.mark.parametrize('POWER_MIN6', [0, 5])
+@pytest.mark.parametrize('POWER_MIN7', [0, 5])
+@pytest.mark.parametrize('POWER_MIN8', [0, 5])
+@pytest.mark.parametrize('POWER_MIN9', [0, 5])
+@pytest.mark.parametrize('POWER_MIN10', [0, 5])
+@pytest.mark.parametrize('POWER_MIN11', [0, 5])
+@pytest.mark.parametrize('POWER_MIN12', [0, 5])
+@pytest.mark.parametrize('POWER_MIN13', [0, 5])
+@pytest.mark.parametrize('POWER_MIN14', [0, 5])
+@pytest.mark.parametrize('POWER_MIN15', [0, 5])
+@pytest.mark.parametrize('POWER_MIN16', [0, 5])
+@pytest.mark.parametrize('POWER_MIN17', [0, 5])
+@pytest.mark.parametrize('POWER_MIN_MAX_DELTA', [4, 10])
+@pytest.mark.parametrize('CHANGE_DIST_VALUE', [1, 5])
+def test_strategy3b(jar_file, cli_path, tmp_path,
+                    CHANGE_POWER_VALUE1, CHANGE_POWER_VALUE2, CHANGE_POWER_VALUE3, CHANGE_POWER_VALUE4,
+                    CHANGE_POWER_VALUE5, CHANGE_POWER_VALUE6, CHANGE_POWER_VALUE7, CHANGE_POWER_VALUE8,
+                    CHANGE_POWER_VALUE9, CHANGE_POWER_VALUE10, CHANGE_POWER_VALUE11, CHANGE_POWER_VALUE12,
+                    CHANGE_POWER_VALUE13, CHANGE_POWER_VALUE14, CHANGE_POWER_VALUE15, CHANGE_POWER_VALUE16,
+                    CHANGE_POWER_VALUE17,
+                    POWER_MIN1, POWER_MIN2, POWER_MIN3, POWER_MIN4, POWER_MIN5, POWER_MIN6, POWER_MIN7, POWER_MIN8,
+                    POWER_MIN9, POWER_MIN10, POWER_MIN11, POWER_MIN12, POWER_MIN13, POWER_MIN14, POWER_MIN15, POWER_MIN16,
+                    POWER_MIN17,
+                    POWER_MIN_MAX_DELTA,
+                    CHANGE_DIST_VALUE
+                    ):
+    strategy = "EAStrategy3b"
+    config = {
+        "CHANGE_POWER_VALUE1": CHANGE_POWER_VALUE1,
+        "CHANGE_POWER_VALUE2": CHANGE_POWER_VALUE2,
+        "CHANGE_POWER_VALUE3": CHANGE_POWER_VALUE3,
+        "CHANGE_POWER_VALUE4": CHANGE_POWER_VALUE4,
+        "CHANGE_POWER_VALUE5": CHANGE_POWER_VALUE5,
+        "CHANGE_POWER_VALUE6": CHANGE_POWER_VALUE6,
+        "CHANGE_POWER_VALUE7": CHANGE_POWER_VALUE7,
+        "CHANGE_POWER_VALUE8": CHANGE_POWER_VALUE8,
+        "CHANGE_POWER_VALUE9": CHANGE_POWER_VALUE9,
+        "CHANGE_POWER_VALUE10": CHANGE_POWER_VALUE10,
+        "CHANGE_POWER_VALUE11": CHANGE_POWER_VALUE11,
+        "CHANGE_POWER_VALUE12": CHANGE_POWER_VALUE12,
+        "CHANGE_POWER_VALUE13": CHANGE_POWER_VALUE13,
+        "CHANGE_POWER_VALUE14": CHANGE_POWER_VALUE14,
+        "CHANGE_POWER_VALUE15": CHANGE_POWER_VALUE15,
+        "CHANGE_POWER_VALUE16": CHANGE_POWER_VALUE16,
+        "CHANGE_POWER_VALUE17": CHANGE_POWER_VALUE17,
+        "POWER_MIN1": POWER_MIN1,
+        "POWER_MIN2": POWER_MIN1,
+        "POWER_MIN3": POWER_MIN1,
+        "POWER_MIN4": POWER_MIN1,
+        "POWER_MIN5": POWER_MIN1,
+        "POWER_MIN6": POWER_MIN1,
+        "POWER_MIN7": POWER_MIN1,
+        "POWER_MIN8": POWER_MIN1,
+        "POWER_MIN9": POWER_MIN1,
+        "POWER_MIN10": POWER_MIN1,
+        "POWER_MIN11": POWER_MIN1,
+        "POWER_MIN12": POWER_MIN1,
+        "POWER_MIN13": POWER_MIN1,
+        "POWER_MIN14": POWER_MIN1,
+        "POWER_MIN15": POWER_MIN1,
+        "POWER_MIN16": POWER_MIN1,
+        "POWER_MIN17": POWER_MIN1,
+        "POWER_MIN_MAX_DELTA": POWER_MIN_MAX_DELTA,
+        "CHANGE_DIST_VALUE": CHANGE_DIST_VALUE,
+    }
+    _execute_simulator(jar_file, cli_path, strategy, config, tmp_path)
+
+
+@pytest.mark.parametrize('CHANGE_POWER_VALUE1', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE2', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE3', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE4', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE5', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE6', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE7', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE8', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE9', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE10', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE11', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE12', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE13', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE14', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE15', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE16', [1,  4])
+@pytest.mark.parametrize('CHANGE_POWER_VALUE17', [1,  4])
+@pytest.mark.parametrize('POWER_MIN1', [0, 5])
+@pytest.mark.parametrize('POWER_MIN2', [0, 5])
+@pytest.mark.parametrize('POWER_MIN3', [0, 5])
+@pytest.mark.parametrize('POWER_MIN4', [0, 5])
+@pytest.mark.parametrize('POWER_MIN5', [0, 5])
+@pytest.mark.parametrize('POWER_MIN6', [0, 5])
+@pytest.mark.parametrize('POWER_MIN7', [0, 5])
+@pytest.mark.parametrize('POWER_MIN8', [0, 5])
+@pytest.mark.parametrize('POWER_MIN9', [0, 5])
+@pytest.mark.parametrize('POWER_MIN10', [0, 5])
+@pytest.mark.parametrize('POWER_MIN11', [0, 5])
+@pytest.mark.parametrize('POWER_MIN12', [0, 5])
+@pytest.mark.parametrize('POWER_MIN13', [0, 5])
+@pytest.mark.parametrize('POWER_MIN14', [0, 5])
+@pytest.mark.parametrize('POWER_MIN15', [0, 5])
+@pytest.mark.parametrize('POWER_MIN16', [0, 5])
+@pytest.mark.parametrize('POWER_MIN17', [0, 5])
+@pytest.mark.parametrize('POWER_MIN_MAX_DELTA', [4, 10])
+@pytest.mark.parametrize('CHANGE_DIST_VALUE_7_8', [1, 5])
+@pytest.mark.parametrize('CHANGE_DIST_VALUE_15_16', [1, 5])
+@pytest.mark.parametrize('CHANGE_DIST_VALUE_5_6', [1, 5])
+def test_strategy3c(jar_file, cli_path, tmp_path,
+                    CHANGE_POWER_VALUE1, CHANGE_POWER_VALUE2, CHANGE_POWER_VALUE3, CHANGE_POWER_VALUE4,
+                    CHANGE_POWER_VALUE5, CHANGE_POWER_VALUE6, CHANGE_POWER_VALUE7, CHANGE_POWER_VALUE8,
+                    CHANGE_POWER_VALUE9, CHANGE_POWER_VALUE10, CHANGE_POWER_VALUE11, CHANGE_POWER_VALUE12,
+                    CHANGE_POWER_VALUE13, CHANGE_POWER_VALUE14, CHANGE_POWER_VALUE15, CHANGE_POWER_VALUE16,
+                    CHANGE_POWER_VALUE17,
+                    POWER_MIN1, POWER_MIN2, POWER_MIN3, POWER_MIN4, POWER_MIN5, POWER_MIN6, POWER_MIN7, POWER_MIN8,
+                    POWER_MIN9, POWER_MIN10, POWER_MIN11, POWER_MIN12, POWER_MIN13, POWER_MIN14, POWER_MIN15, POWER_MIN16,
+                    POWER_MIN17,
+                    POWER_MIN_MAX_DELTA,
+                    CHANGE_DIST_VALUE_7_8, CHANGE_DIST_VALUE_15_16, CHANGE_DIST_VALUE_5_6
+                    ):
+    strategy = "EAStrategy3c"
+    config = {
+        "CHANGE_POWER_VALUE1": CHANGE_POWER_VALUE1,
+        "CHANGE_POWER_VALUE2": CHANGE_POWER_VALUE2,
+        "CHANGE_POWER_VALUE3": CHANGE_POWER_VALUE3,
+        "CHANGE_POWER_VALUE4": CHANGE_POWER_VALUE4,
+        "CHANGE_POWER_VALUE5": CHANGE_POWER_VALUE5,
+        "CHANGE_POWER_VALUE6": CHANGE_POWER_VALUE6,
+        "CHANGE_POWER_VALUE7": CHANGE_POWER_VALUE7,
+        "CHANGE_POWER_VALUE8": CHANGE_POWER_VALUE8,
+        "CHANGE_POWER_VALUE9": CHANGE_POWER_VALUE9,
+        "CHANGE_POWER_VALUE10": CHANGE_POWER_VALUE10,
+        "CHANGE_POWER_VALUE11": CHANGE_POWER_VALUE11,
+        "CHANGE_POWER_VALUE12": CHANGE_POWER_VALUE12,
+        "CHANGE_POWER_VALUE13": CHANGE_POWER_VALUE13,
+        "CHANGE_POWER_VALUE14": CHANGE_POWER_VALUE14,
+        "CHANGE_POWER_VALUE15": CHANGE_POWER_VALUE15,
+        "CHANGE_POWER_VALUE16": CHANGE_POWER_VALUE16,
+        "CHANGE_POWER_VALUE17": CHANGE_POWER_VALUE17,
+        "POWER_MIN1": POWER_MIN1,
+        "POWER_MIN2": POWER_MIN1,
+        "POWER_MIN3": POWER_MIN1,
+        "POWER_MIN4": POWER_MIN1,
+        "POWER_MIN5": POWER_MIN1,
+        "POWER_MIN6": POWER_MIN1,
+        "POWER_MIN7": POWER_MIN1,
+        "POWER_MIN8": POWER_MIN1,
+        "POWER_MIN9": POWER_MIN1,
+        "POWER_MIN10": POWER_MIN1,
+        "POWER_MIN11": POWER_MIN1,
+        "POWER_MIN12": POWER_MIN1,
+        "POWER_MIN13": POWER_MIN1,
+        "POWER_MIN14": POWER_MIN1,
+        "POWER_MIN15": POWER_MIN1,
+        "POWER_MIN16": POWER_MIN1,
+        "POWER_MIN17": POWER_MIN1,
+        "POWER_MIN_MAX_DELTA": POWER_MIN_MAX_DELTA,
+        "CHANGE_DIST_VALUE_7_8": CHANGE_DIST_VALUE_7_8,
+        "CHANGE_DIST_VALUE_15_16": CHANGE_DIST_VALUE_15_16,
+        "CHANGE_DIST_VALUE_5_6": CHANGE_DIST_VALUE_5_6,
+    }
+    _execute_simulator(jar_file, cli_path, strategy, config, tmp_path)
