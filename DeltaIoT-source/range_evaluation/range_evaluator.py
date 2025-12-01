8b416a0b1ea5ed70d7ff1c846b61fbf83d300cb6
Ralf Sieger <sieger@fzi.de>
2025-12-01 15:56:42 +0100

    added strategy selection

--- DeltaIoT-source/range_evaluation/range_evaluator.py
+++ DeltaIoT-source/range_evaluation/range_evaluator.py
@@ -8,6 +8,7 @@ import statistics
 
 from tabulate import tabulate
 
+from strategy_kind import StrategyKind
 from sample_estimator import calculate_required_samples
 
 
@@ -38,18 +39,15 @@ class RangeEvaluator:
             result = json.load(f)
             return result
 
-    def _execute_simulator(self, strategy, config, tmp_path):
-        strategy_conf = None
-        if strategy:
-            strategy_conf = tmp_path / ("%s.json" % strategy)
-            with strategy_conf.open("w", encoding="utf-8") as f:
-                f.write(json.dumps(config, indent=2))
+    def _execute_simulator(self, strategy: StrategyKind, tmp_path):
+        strategy_conf = tmp_path / ("%s.json" % "empty")
+        with strategy_conf.open("w", encoding="utf-8") as f:
+            f.write(json.dumps({}, indent=2))
 
         result_file = "result.json"
         args = ["-jar", self._jar_file, "-r", result_file]
         args.append("--no_validation")
-        if strategy:
-            args.extend([ "strategy", "-a", strategy, "-p", strategy_conf])
+        args.extend(strategy.extra_arguments)
 
         proc = self._run_cli(args, cli=self._java_path, cwd=str(tmp_path))
         if proc.returncode != 0:
@@ -60,23 +58,22 @@ class RangeEvaluator:
         result = self._read_result_file(expected_result_file)
         return result
 
-    def _collect_samples(self, count):
+    def _collect_samples(self, count, strategy: StrategyKind):
         samples = []
         for i in range(0, count):
             with tempfile.TemporaryDirectory() as tmpdir_name:
-                strategy = None
-                config = {}
-                result = self._execute_simulator(strategy, config, Path(tmpdir_name))
+                result = self._execute_simulator(strategy, Path(tmpdir_name))
                 samples.append(result["statistics"])
-                #print(f"result: {json.dumps(result["statistics"], indent=2)}")
         return samples
 
     def main(self):
         parser = argparse.ArgumentParser(prog="range_evaluator", description="Establish DeltaIoT range boundaries")
         default = ' (default: %(default)s)'
-        #parser.add_argument('-r', '--result', type=as_path, help="result json file")
         parser.add_argument('--confidence', type=int, default=95, help="confidence in percent" + default)
         parser.add_argument('--accuracy', type=float, default=0.01, help="accuracy of result" + default)
+        parser.add_argument('-t', '--type',
+                            choices=[_type.type.lower() for _type in StrategyKind],
+                            default=StrategyKind.NONE.type.lower(), help="select adaption strategy type" + default)
         args = parser.parse_args()
 
         if not self._java_path:
@@ -84,9 +81,11 @@ class RangeEvaluator:
         if not self._jar_file:
             raise RuntimeError("unable to find: %s" % self._jar_file)
 
+        strategy = StrategyKind[args.type.upper()]
         sample_count = calculate_required_samples(args.confidence, args.accuracy)
         print(f"sample count for confidence = {args.confidence}% and accuracy = {args.accuracy}: {sample_count}")
-        samples = self._collect_samples(sample_count)
+        print(f"using adaption strategy: {strategy.type}")
+        samples = self._collect_samples(sample_count, strategy)
         energy_consumption_min = min([sample["energyConsumption"]["min"] for sample in samples])
         energy_consumption_max = max([sample["energyConsumption"]["max"] for sample in samples])
         energy_consumption_average = statistics.mean([sample["energyConsumption"]["average"] for sample in samples])
---
+++ DeltaIoT-source/range_evaluation/strategy_kind.py
@@ -0,0 +1,13 @@
+from enum import Enum
+from dataclasses import dataclass
+
+
+@dataclass
+class Input:
+    type: str
+    extra_arguments: list
+
+
+class StrategyKind(Input, Enum):
+    NONE = "none", []
+    DEFAULT = "default", ["strategy", "-a", "Default", "-p", "empty.json"]
