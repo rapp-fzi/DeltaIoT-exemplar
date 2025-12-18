e976bbacfdcfbef95a6143beda0e56098fa8bf68
Ralf Sieger <sieger@fzi.de>
2025-12-18 09:06:54 +0100

    added new strategies

--- validate_simexp/strategy.py
+++ validate_simexp/strategy.py
@@ -2,6 +2,12 @@ from enum import Enum
 
 
 class Strategy(Enum):
+    EAStrategy0a = "EAStrategy0a"
+    EAStrategy0b = "EAStrategy0b"
+    EAStrategy0c = "EAStrategy0c"
+    EAStrategy0d = "EAStrategy0d"
     EAStrategy1a = "EAStrategy1a"
     EAStrategy1b = "EAStrategy1b"
     EAStrategy1c = "EAStrategy1c"
+    EAStrategy1d = "EAStrategy1d"
+    EAStrategy2d = "EAStrategy2d"
