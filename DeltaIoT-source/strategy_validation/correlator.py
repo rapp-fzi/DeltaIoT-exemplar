ff36ef1b3f11378bb6564b4cf276533e6dbc0f38
Ralf Sieger <sieger@fzi.de>
2026-01-14 09:03:50 +0100

    added detection of aborted tasks

--- DeltaIoT-source/strategy_validation/correlator.py
+++ DeltaIoT-source/strategy_validation/correlator.py
@@ -19,7 +19,9 @@ class Correlator:
         all_tasks = []
         for result_file in args.task_file:
             stats = self._analyze_task_result(result_file)
-            all_tasks.append(stats)
+            if stats["status"] != "ABORT":
+                all_tasks.append(stats)
+        print(f"completed tasks:    {len(all_tasks)}")
 
         energy_normalizer = Normalizer(9, 26)
         packet_loss_normalizer = Normalizer(0.02, 0.4)
@@ -29,18 +31,21 @@ class Correlator:
         all_data = []
         simulator = Simulator()
         simulator.init()
-        for task_id, optimizables, reward, reward_type, runs in all_tasks:
+        for task_data in all_tasks:
             with tempfile.TemporaryDirectory() as tmpdir_name:
                 tmp_path = Path(tmpdir_name)
-                config_file = self._create_strategy_conf(tmp_path, args.strategy, optimizables)
-                strat_id = "%s:%s" % (args.strategy.name, task_id)
+                config_file = self._create_strategy_conf(tmp_path, args.strategy, task_data["optimizables"])
+                strat_id = "%s:%s" % (args.strategy.name, task_data["id"])
                 data = simulator.collect_simulation_data(simulator, args.runs, strat_id, args.strategy, config_file, args.seed, args.max_workers)
                 score_average = statistics.mean([result[1] for result in data])
                 if args.calc_average_reward:
                     reward_type = "AVERAGE"
-                    reward = total_reward_calculator.total_reward(runs)
+                    reward = total_reward_calculator.total_reward(task_data["runs"])
+                else:
+                    reward = task_data["reward"]
+                    reward_type = task_data["reward_type"]
 
-                all_data.append((task_id, reward, reward_type, score_average, optimizables))
+                all_data.append((task_data["id"], reward, reward_type, score_average, task_data["optimizables"]))
 
         table_entries = []
         for task_id, reward, reward_type, score_average, optimizables in all_data:
@@ -75,25 +80,27 @@ class Correlator:
 
     def _analyze_task_result(self, result_file):
         task_result = self._read_json_file(result_file)
-        task_id = task_result["result"]["id"]
-        reward = task_result["result"]["reward"]
-        reward_type = task_result["result"]["reward_type"]
-        optimizables = task_result["optimizables"]
-        runs = task_result["result"]["quality_measurements"]["runs"]
-        #energy_consumption = []
-        #packet_loss = []
-        #for run in runs:
-        #    qas = run["quality_attributes"]
-        #    energy_consumption.extend(qas["EnergyConsumption.props"])
-        #    packet_loss.extend(qas["PacketLoss.props"])
+        try:
+            task_id = task_result["result"]["id"]
+            status = task_result["result"]["status"]
+            reward = task_result["result"]["reward"]
+            reward_type = task_result["result"]["reward_type"]
+            optimizables = task_result["optimizables"]
+            if task_result["result"]["quality_measurements"]:
+                runs = task_result["result"]["quality_measurements"]["runs"]
+            else:
+                runs = []
+        except Exception as e:
+            raise RuntimeError(f"file {result_file}") from e
 
-        result = (
-            task_id,
-            optimizables,
-            reward,
-            reward_type,
-            runs,
-        )
+        result = {
+            "id": task_id,
+            "status": status,
+            "optimizables": optimizables,
+            "reward": reward,
+            "reward_type": reward_type,
+            "runs": runs,
+        }
         return result
 
     def _read_json_file(self, json_file):
