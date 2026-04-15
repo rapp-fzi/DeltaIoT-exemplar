c00cd7f70a65471a42dd930037963e1646dd31f2
Ralf Sieger <sieger@fzi.de>
2026-04-15 09:40:12 +0200

    added progress display

--- validate_simexp/score_builder.py
+++ validate_simexp/score_builder.py
@@ -1,3 +1,5 @@
+from tqdm import tqdm, trange
+
 from simulator import Simulator
 
 
@@ -14,7 +16,8 @@ class ScoreBuilder:
 
     def _build_score(self, strategy, seed, no_validation, values, count):
         scores = []
-        for i in range(count):
+        bar_format = "{l_bar}{bar}| {n_fmt}/{total_fmt} {rate_fmt}"
+        for i in trange(count, leave=False, colour="blue", bar_format=bar_format, unit="simulation"):
             score = self._simulator.simulate(strategy, values, seed, no_validation)
             scores.append(score)
         return scores
@@ -23,7 +26,9 @@ class ScoreBuilder:
         groups = self._group_entries(entries)
 
         score_entries = {}
-        for group, value in groups.items():
+        bar_format = "{l_bar}{bar}| {n_fmt}/{total_fmt} {rate_fmt}"
+        items = tqdm(groups.items(), colour="green", bar_format=bar_format, unit="generation")
+        for group, value in items:
             values = value[0]["Values"]
             scores = self._build_score(strategy, seed, no_validation, values, count)
             average_score = sum(scores) / len(scores)
